49da2f2c2fa0ff4fd17e7f3bf967318e74af3e23
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-10-08 13:22:30 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+const broncosRec = [
+    { year: 2017, result: "N/A" },
+    { year: 2016, result: "N/A" },
+    { year: 2015, result: "W" }
+];
+
+function superbowlWin(record) {
+
+    const winRecord = record.find(item => item.result === "W");
+
+
+    if (winRecord) {
+        return winRecord.year.toString();
+    }
+
+
+    return undefined;
+}
+
+
+console.log(superbowlWin(broncosRec)); 
